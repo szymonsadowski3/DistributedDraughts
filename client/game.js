97312af443ba2a02e8223e3caa2066208da934ce
Szymon <szymonsadowski3@gmail.com>
2018-01-02 17:41:07 +0100

    started working on displaying

---
+++ client/game.js
@@ -0,0 +1,25 @@
+var loc = "http://127.0.0.1:8080/board";
+var COLS = 10;
+
+$(document).ready(function() {
+  $.get(loc, function(data) {
+    var parsed_array = JSON.parse(data);
+    updateBoard(parsed_array);
+  });
+});
+
+function updateBoard(board_data) {
+  var table = $(".board").find("table")[0];
+
+  console.dir(table.rows[0]);
+
+  for (var i = 0; i < COLS; ++i) {
+    for (var j = 0; j < COLS; ++j) {
+      if (board_data[i * COLS + j] == 1) {
+        table.rows[i].children[j].innerHTML = '<div class="red_piece"></div>';
+      } else if (board_data[i * COLS + j] == 2) {
+        table.rows[i].children[j].innerHTML = '<div class="black_piece"></div>';
+      }
+    }
+  }
+}
